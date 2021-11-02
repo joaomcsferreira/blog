d969a88f68d559b6321b839e27c401712e3b8907
Joao-MCF <marcosc024@gmail.com>
2021-11-02 09:25:10 -0300

    Displaying SVGs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,10 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.assets.precompile += %w( '.svg' )
+
   # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
